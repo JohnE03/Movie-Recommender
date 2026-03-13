cc80cc823e0219927ef159c63a2c10f2c3a9121e
JohnE03 <167023195+JohnE03@users.noreply.github.com>
2026-03-13 14:42:48 +0200

    Add files via upload

---
+++ src/User.java
@@ -0,0 +1,65 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class User {
+    private String userName;
+    private String userId;
+    private List<String> moviesId; //movie IDs
+
+    private List<Movie> userMovies = new ArrayList<>();
+    private Set<String> userGenres = new HashSet<>();
+
+    static List<User> usersList = new ArrayList<>();
+
+    public User(String userName, String userId, List<String> moviesId) {
+        this.userName = userName;
+        this.userId = userId;
+        this.moviesId = moviesId;
+        getUserMovies();
+        usersList.add(this);
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+    public String getUserId() {
+        return userId;
+    }
+    public List<String> getMoviesId() {
+        return moviesId;
+    }
+    public static List<User> getUsersList(){return usersList;}
+
+    public static void clearUsersList(){usersList.clear();}
+
+    public void getUserMovies(){
+        for(String movieId:this.moviesId){
+            for(Movie movie:Movie.getMovies()){
+                if(movie.getId().equals(movieId)){
+                    this.userMovies.add(movie);
+                    this.userGenres.addAll(movie.getGenre());
+                }
+            }
+        }
+    }
+
+    public List<Movie> getRecommendations(){
+        List<Movie> recommendations=new ArrayList<>();
+
+        for(Movie movie:Movie.getMovies()){
+            if(moviesId.contains(movie.getId())) continue;
+
+            for(String genre:movie.getGenre()){
+                if(userGenres.contains(genre)){
+                    recommendations.add(movie);
+                    break;
+                }
+            }
+
+        }
+
+        return recommendations;
+    }
+}
